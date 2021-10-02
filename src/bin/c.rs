b38af2d5d3a5903d62ebc32d59f6faa75c872d2f
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-02 15:16:10 +0900

    init

---
+++ src/bin/c.rs
@@ -0,0 +1,55 @@
+#[allow(unused_macros)]
+macro_rules! parse_line {
+    ( $t:ty ) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            iter.next().unwrap().parse::<$t>().unwrap()
+        }
+    );
+
+    ( $( $t:ty), +) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            ( $(iter.next().unwrap().parse::<$t>().unwrap()),* )
+        }
+    );
+}
+
+#[allow(unused_macros)]
+macro_rules! read_line {
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! parse_vec {
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
+}
+
+fn main() {
+    let mut n = parse_line!(u64);
+    let mut ans = Vec::new();
+    while n != 0 {
+        if n % 2 == 0 {
+            ans.push("B".to_string());
+            n /= 2;
+        } else {
+            ans.push("A".to_string());
+            n -= 1;
+        }
+    }
+    println!("{}", ans.into_iter().rev().collect::<String>());
+}
